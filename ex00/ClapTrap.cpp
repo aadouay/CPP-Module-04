42cd389ab08de205df7529f31d28d4a7e0428cf0
Ayoub Adouay <ayadouay@c4r10p3.1337.ma>
2025-10-22 16:39:25 +0100

    add

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,7 +1,9 @@
 #include "ClapTrap.hpp"
 
 ClapTrap::ClapTrap():name("NULL"), Hit_points(10),
-                    Energy_points(10), Attack_damage(0){}
+                    Energy_points(10), Attack_damage(0){
+                        std::cout << "the default constractor is called !\n";
+                    }
 
 ClapTrap::ClapTrap(std::string name){
     this->name = name;
@@ -15,19 +17,47 @@ void    ClapTrap::attack(const std::string& target){
         std::cout << "ClapTrap " << name << " can't attack, no hit points left!\n";
         return;
     }
+
     if (Energy_points <= 0) {
         std::cout << "ClapTrap " << name << " can't attack, no energy points left!\n";
         return;
     }
-    Energy_points = Energy_points - 1;
+
+    Energy_points--;
     std::cout << "ClapTrap " << name << " attacks " << target 
               << ", causing " << Attack_damage << " points of damage!\n";
 }
 
 void    ClapTrap::takeDamage(unsigned int amount){
+    if (Hit_points <= 0) {
+        std::cout << "ClapTrap " << name << " is already destroyed!\n";
+        return;
+    }
 
+    if (amount >= (unsigned int)Hit_points)
+        Hit_points = 0;
+    else
+        Hit_points -= amount;
+    std::cout << "ClapTrap " << name << " takes " << amount
+              << " damage! Remaining HP: " << Hit_points << std::endl;
 }
 
 void    ClapTrap::beRepaired(unsigned int amount){
+    if (Hit_points <= 0) {
+        std::cout << "ClapTrap " << name << " is already destroyed!\n";
+        return;
+    }
 
+    if(Energy_points <= 0){
+        std::cout << "ClapTrap " << name << " can't be repaired, no energy points left!\n";
+        return;
+    }
+
+    Energy_points--;
+    Hit_points+= amount;
+    std::cout << "ClapTrap " << name << "be repaired ! Remaining HP: " << Hit_points << std::endl;
+}
+
+ClapTrap::~ClapTrap(){
+    std::cout << "the deconstractor is called!\n";
 }
---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+#include "ClapTrap.hpp"
+
+int main(){
+    ClapTrap obj;
+
+    obj.attack("yassin");
+    obj.takeDamage(5);
+    obj.attack("yasin");
+    obj.takeDamage(4);
+    obj.attack("yassing");
+    obj.attack("yassing");
+    obj.attack("yassing");
+    obj.attack("yassing");
+    obj.attack("yassing");
+    obj.attack("yassing");
+    obj.attack("yassing");
+    // obj.attack("yassing");
+    // obj.attack("yassing");
+    // obj.attack("yassing");
+    // obj.attack("yassing");
+    obj.beRepaired(5);
+    obj.beRepaired(5);
+    obj.attack("yassing");
+    obj.attack("yassing");
+    obj.takeDamage(45);
+    obj.attack("ysing");
+}
